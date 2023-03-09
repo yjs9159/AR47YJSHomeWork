11cc47ada6b6ff8f2cabee47ae92aaf0d0d4187f
종서 윤 <yjs9159@naver.com>
2023-03-10 03:03:00 +0900

    윤종서 : 컴파일, 진입점, 변수선언, 연산자 준비

--- HomeWork/004_Var/004_Var.cpp
+++ HomeWork/004_Var/004_Var.cpp
@@ -1,11 +1,11 @@
-﻿// 001_AR47Start.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// 004_Var.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World@!\n";
+    std::cout << "Hello World!\n";
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
