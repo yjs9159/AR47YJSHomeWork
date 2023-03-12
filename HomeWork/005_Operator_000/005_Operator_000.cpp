cc0e540fd06bd700bb23e1b7ebe2ebd9b0d74ad5
종서 윤 <yjs9159@naver.com>
2023-03-13 02:24:43 +0900

    윤종서 : 연산자 대입 및 초기화 , 비트

---
+++ HomeWork/005_Operator_000/005_Operator_000.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int Left = 7;
+    int Right = 3;
+    int Result = 0;
+
+    
+    Result = Left + Right;
+    Result = Left - Right;
+    Result = Left * Right;
+    Result = Left / Right;
+
+    bool bResult = true;
+}
