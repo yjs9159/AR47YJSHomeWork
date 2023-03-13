f2593a79d997d8cdeb83e76f581020862b04fb81
종서 윤 <yjs9159@naver.com>
2023-03-13 23:36:55 +0900

    윤종서 : 연산자 마무리 및 함수 기초

---
+++ HomeWork/010__Operator003/011__Operator003.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+
+    /* 
+    삼항 연산자
+    a ? b : c
+    참 혹은 거짓 메모리 ? 참일때 메모리 : 거짓일때 메모리
+    */
+
+    int Left = 7;
+    int Right = 3;
+    int Result = 0;
+
+    Result = (Left != Right) ? 100 : 200;
+    Result = true ? 100 : 200;
+
+    
+    /*
+    sizeof 연산자
+    ()안에 있는 메모리 영역의 바이트 크기를 리턴
+    sizeof(int)
+    sizeof(bool)
+    */
+    Result = sizeof(Left);
+    Result = sizeof(int);
+    Result = sizeof(bool);
+
+
+}
