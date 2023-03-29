a4c66742bc8b0b73f58fa10cb5ba2d70101120ef
종서 윤 <yjs9159@naver.com>
2023-03-30 00:49:00 +0900

    0330 3차

--- HomeWork/HomeWork0329/HomeWork0329.cpp
+++ HomeWork/HomeWork0329/HomeWork0329.cpp
@@ -1,18 +1,48 @@
-﻿#include <iostream>
+﻿// HomeWork0329.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include "Player.h"
+#include "ConsoleGameScreen.h"
 
 int main()
 {
-	// 총알 1말을 발사하기
-	// 방향 상관없음
-	// 총알 클래스를 만든다
-	// 플레이어가 f (지정한) 키를 누르면
+	// 오늘의 숙제입니다.
+	// 총알 1발을 발사하세요.
+	// 방향 상관 없음.
+	// 총알 클래스를 만든다.
+	// 플레이어가 여러분들이 지정한 키를 누르르면 
 
-	// Buller NewBullet;
-	// Has a 구조 Player Has a Bullet
-	// bool Fire = false;
+	// Bullet NewBullet;
 
-	// inline bool IsFire()
-	/*{
-		return fire;
-	}*/
+	Player NewPlayer = Player();
+	int2 ScreenSize = ConsoleGameScreen::GetMainScreen().GetScreenSize();
+	NewPlayer.SetPos(ScreenSize.Half());
+
+	while (true)
+	{
+		system("cls");
+
+		ConsoleGameScreen::GetMainScreen().ScreenClear();
+
+		ConsoleGameScreen::GetMainScreen().SetScreenCharacter(NewPlayer.GetPos(), '*');
+
+		ConsoleGameScreen::GetMainScreen().ScreenPrint();
+
+		NewPlayer.Input();
+
+	}
+
+	std::cout << "Hello World!\n";
 }
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
